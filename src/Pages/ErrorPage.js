77f9c9e1c09ef40d17da09a8c15c50bc583dd159
inventor321 <all32189000@gmail.com>
2022-07-04 19:15:19 -0400

    Fix issue routing part 2

--- src/Pages/ErrorPage.js
+++ src/Pages/ErrorPage.js
@@ -2,7 +2,13 @@ import React from "react";
 import Tabs from "../Widgets/Tabs";
 
 function ErrorPage() {
+
+  
+  console.log(window.location.href);
+
   return <body >PAGE DOES NOT EXIST
+      <h1>Hi! Try edit me {window.location.href}</h1>;
+      <h1>Hi! Try edit me {window.location.pathname}</h1>;
       <Tabs></Tabs>
   </body>;
   
